1a58c7b63339eac28cb2d38b8029dba0dc7ada32
Akashdeep Nandi <adncorwin@gmail.com>
2020-09-01 00:02:59 +0530

    README modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,29 +71,11 @@
                     <!-- /.col -->
                 </div>
             </form>
-
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using
-                    Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in
-                    using Google+
-                </a>
-            </div>
-            <!-- /.social-auth-links -->
             @if (Route::has('password.request'))
             <p class="mb-1">
                 <a href="{{ route('password.request') }}">I forgot my password</a>
             </p>
             @endif
-            <p class="mb-0">
-                <a href="register.html" class="text-center"
-                    >Register a new membership</a
-                >
-            </p>
         </div>
         <!-- /.login-card-body -->
     </div>
